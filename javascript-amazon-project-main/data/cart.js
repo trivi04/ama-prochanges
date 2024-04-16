2c26c385ee2cc633ea6360060d89203afba00567
trivikram <trivikramsai04@gmail.com>
2024-04-16 10:50:52 +0530

    made checkout interactive

--- javascript-amazon-project-main/data/cart.js
+++ javascript-amazon-project-main/data/cart.js
@@ -1,30 +1,92 @@
-export const cart=[{
-    productId:"e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
-    quantity:2,
-},
-{
-    productId:"15b6fc6f-327a-4ec4-896f-486349e85a3d",
-    quantity:1,
+// export const cart=[{
+//     productId:"e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
+//     quantity:2,
+// },
+// {
+//     productId:"15b6fc6f-327a-4ec4-896f-486349e85a3d",
+//     quantity:1,
+// }
+// ];
+
+// export function addToCart(productId){
+//     let matchingItem;
+//     cart.forEach((cartItem) => {
+//         if (productId === cartItem.productId) {
+//             matchingItem = cartItem;
+//         }
+//     });
+
+//     const quantitySelector = document.querySelector(`.js-quantity-selector[data-product-id="${productId}"]`);
+//     const quantity = Number(quantitySelector.value);
+
+//     if (matchingItem) {
+//         matchingItem.quantity += quantity;
+//     } else {
+//         cart.push({
+//             productId,
+//             quantity
+//         });
+//     }
+// }
+
+// export function removeFromCart(productId){
+//     const newCart=[];
+//     cart.forEach((cartItem)=>{
+//         if(cartItem.productId !== productId){
+//             newCart.push(cartItem);
+//         }
+//     });
+
+//     cart=newCart;
+// }
+
+export let cart = JSON.parse(localStorage.getItem('cart'));
+
+if (!cart) {
+  cart = [{
+    productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
+    quantity: 2,
+  }, {
+    productId: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
+    quantity: 1
+  }];
 }
-];
 
-export function addToCart(productId){
-    let matchingItem;
-    cart.forEach((cartItem) => {
-        if (productId === cartItem.productId) {
-            matchingItem = cartItem;
-        }
-    });
+function saveToStorage() {
+  localStorage.setItem('cart', JSON.stringify(cart));
+}
 
-    const quantitySelector = document.querySelector(`.js-quantity-selector[data-product-id="${productId}"]`);
-    const quantity = Number(quantitySelector.value);
+export function addToCart(productId) {
+  let matchingItem;
 
-    if (matchingItem) {
-        matchingItem.quantity += quantity;
-    } else {
-        cart.push({
-            productId,
-            quantity
-        });
+  cart.forEach((cartItem) => {
+    if (productId === cartItem.productId) {
+      matchingItem = cartItem;
     }
+  });
+
+  if (matchingItem) {
+    matchingItem.quantity += 1;
+  } else {
+    cart.push({
+      productId: productId,
+      quantity: 1
+    });
+  }
+
+  saveToStorage();
+}
+
+export function removeFromCart(productId) {
+  const newCart = [];
+
+  cart.forEach((cartItem) => {
+    if (cartItem.productId !== productId) {
+      newCart.push(cartItem);
+    }
+  });
+
+  cart = newCart;
+
+  saveToStorage();
 }
