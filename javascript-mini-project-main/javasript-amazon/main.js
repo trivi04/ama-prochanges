acaba938a0f037eb8c9897f2fde5554010f94025
trivikram <trivikramsai04@gmail.com>
2024-04-26 11:26:44 +0530

    changes

--- javascript-mini-project-main/javasript-amazon/main.js
+++ javascript-mini-project-main/javasript-amazon/main.js
@@ -19,18 +19,9 @@ function generateProductHTML(products) {
                 &#8377 ${formatCurrency(product.priceCents)}
                 </div>
                 <div class="product-quantity-container">
-                    <select class="js-quantity-selector" data-product-id="${product.id}">
-                        <option selected value="1">1</option>
-                        <option value="2">2</option>
-                        <option value="3">3</option>
-                        <option value="4">4</option>
-                        <option value="5">5</option>
-                        <option value="6">6</option>
-                        <option value="7">7</option>
-                        <option value="8">8</option>
-                        <option value="9">9</option>
-                        <option value="10">10</option>
-                    </select>
+                <div class="js-quantity-selector" data-product-id="${product.id}">
+                    
+                    </div>
                 </div>
                 <div class="product-spacer"></div>
                 <div class="added-to-cart js-added-to-cart" data-product-id="${product.id}">
